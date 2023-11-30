7092a28422c34df75eb7d2f798c2b96b0c1b6faa
sserh <serhios@bk.ru>
2023-11-30 14:08:26 +0700

    Program is ready

--- src/main/java/ru/raccoon/Main.java
+++ src/main/java/ru/raccoon/Main.java
@@ -1,7 +1,102 @@
 package ru.raccoon;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        ArrayBlockingQueue<String> arrayBlockingQueueForA = new ArrayBlockingQueue<>(100);
+        ArrayBlockingQueue<String> arrayBlockingQueueForB = new ArrayBlockingQueue<>(100);
+        ArrayBlockingQueue<String> arrayBlockingQueueForC = new ArrayBlockingQueue<>(100);
+
+        new Thread(() -> {
+            for (int i = 0; i < 10_000; i++) {
+                String str = generateText("abc", 100_000);
+                try {
+                    arrayBlockingQueueForA.put(str);
+                    arrayBlockingQueueForB.put(str);
+                    arrayBlockingQueueForC.put(str);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                }
+
+            }
+        }).start();
+
+        new Thread(() -> {
+            int count = 0;
+            String stringWithMaxCount = "";
+            for (int i = 0; i < 10_000; i++) {
+                String tmp;
+                try {
+                    tmp = arrayBlockingQueueForA.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                int tmpLength = getSymbolCount(tmp, "a");
+                if (tmpLength > count) {
+                    count = tmpLength;
+                    stringWithMaxCount = tmp;
+                }
+            }
+            printResult("a", count, stringWithMaxCount);
+        }).start();
+
+        new Thread(() -> {
+            int count = 0;
+            String stringWithMaxCount = "";
+            for (int i = 0; i < 10_000; i++) {
+                String tmp;
+                try {
+                    tmp = arrayBlockingQueueForB.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                int tmpLength = getSymbolCount(tmp, "b");
+                if (tmpLength > count) {
+                    count = tmpLength;
+                    stringWithMaxCount = tmp;
+                }
+            }
+            printResult("b", count, stringWithMaxCount);
+        }).start();
+
+        new Thread(() -> {
+            int count = 0;
+            String stringWithMaxCount = "";
+            for (int i = 0; i < 10_000; i++) {
+                String tmp;
+                try {
+                    tmp = arrayBlockingQueueForC.take();
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+                int tmpLength = getSymbolCount(tmp, "c");
+                if (tmpLength > count) {
+                    count = tmpLength;
+                    stringWithMaxCount = tmp;
+                }
+            }
+            printResult("c", count, stringWithMaxCount);
+        }).start();
+
+    }
+
+    public static String generateText(String letters, int length) {
+        Random random = new Random();
+        StringBuilder text = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            text.append(letters.charAt(random.nextInt(letters.length())));
+        }
+        return text.toString();
+    }
+
+    //метод подсчёта символов в строке
+    public static int getSymbolCount(String str, String symbol) {
+        return str.length() - str.replace(symbol, "").length();
+    }
+
+    public static void printResult(String symbol, int count, String resultString) {
+        System.out.println("\nСтрока с максимальным количеством символов " + symbol + " (" + count + " вхождений): " + resultString);
     }
 }
